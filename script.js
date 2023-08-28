13339d766728798b425c221721a22229c93fe660
Colin Macrae <csmacrae@gmail.com>
2023-08-28 14:29:03 -0700

    comments update

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-// function showAlert() {
-//   window.alert(
-// "alert"  );
-// }
-// showAlert()
+function showAlert() {
+  window.alert(
+    "Below this section is my Omnifood CSS portfolio project site, which is a work in progress and is almost fully finished (for desktop dimensions). As of 8/21/2023 the desktop version should be finished within four days, and the mobile optimization should be finished around 9/7. Thanks for visiting!-Colin 8/24 update -- desktop optimization completed 8/25 update -- mobile dimension responsiveness in progress (completed for small laptops and landscape tablets and tablets today). there may be some broken parts atm8/28 update -- phone dimension responsiveness completed. javascript to still be added for menu functionality. other updates to be made. project estimated completion date: 9/2 Other notes: This is a CSS & HTML project and has no interactive functionality. It is currently being built for the purpose of showcasing my CSS & HTML skills only. Please see my Pinocelli project (link below) to see my interactive, full-stack ReactJS/JavaScript application which has full frontend and backend functionality. → Click here for the Pinocelli site >> https://pinocelli-app.azurewebsites.net/"
+  );
+}
+showAlert()
