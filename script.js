9c41b0af27fdd1d8000f9b1080ff9269a3df9e28
Colin Macrae <csmacrae@gmail.com>
2023-08-29 14:11:16 -0700

    sticky navbar functionality added

--- script.js
+++ script.js
@@ -18,4 +18,26 @@ btnNavEl.addEventListener('click', function() {
   headerEl.classList.toggle('nav-open');
 })
 
+// sticky navigation 
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const observer = new IntersectionObserver(function (entries) {
+  const ent = entries[0];
+  console.log(ent);
+
+  if(ent.isIntersecting === false) {
+    document.body.classList.add('sticky')
+  }  
+  if (ent.isIntersecting) {
+    document.body.classList.remove("sticky");
+  }
+}, {
+  // in viewport
+  root: null, 
+  threshold: 0,
+  rootMargin: "-80px"
+}
+);
+observer.observe(sectionHeroEl);
+
 
