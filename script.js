543d2b966f8ff5935084cb7166995db29e73583b
Colin Macrae <csmacrae@gmail.com>
2023-08-29 11:01:03 -0700

    mobile nav menu functionality completed

--- script.js
+++ script.js
@@ -5,8 +5,17 @@ function showAlert() {
 }
 // showAlert()
 
+// set current year 
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+// make mobile nav work 
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener('click', function() {
+  headerEl.classList.toggle('nav-open');
+})
 
 
